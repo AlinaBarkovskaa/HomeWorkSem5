ff39509596e0f364d99f9a032a3c5b0cdfb57d37
AlinaBarkovskaa <a.barkovskaa@icloud.com>
2022-12-06 10:47:00 +0400

    added task

---
+++ task34/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] GetArrayRandomNumbers(int n)
+{
+   int[] array = new int[n];
+   for (int i = 0; i < array.Length; i++)
+   {
+       array[i] = new Random().Next(100,1000);
+   }
+   return array;
+}
+int GetCountEvenNumbers(int[] array)
+{
+    int count = 0;
+  for(int i = 0; i < array.Length; i++)
+  {
+     if(array[i] % 2 == 0)
+     {
+       count++;
+     }
+  }
+  return count;
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] mas = GetArrayRandomNumbers(n);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", mas)}]");
+int rezult = GetCountEvenNumbers(mas);
+Console.WriteLine($"Четных чисел в массиве = {rezult}");
